70af248fef2070b7994ddb1b7ea467f1b1c09137
roman <romanzchov@mail.ru>
2024-06-14 00:14:34 +0300

    Home Work - 421

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -14,24 +14,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static java.nio.file.Paths.get;
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
-
 @Service
 public class AvatarService {
     private final StudentService studentService;
     private final AvatarRepository avatarRepository;
-
     @Value("${avatars.dir.path}")
     private String avatarsDir;
-
-    public AvatarService(StudentService studentService, AvatarRepository avatarRepository, AvatarService avatarService) {
+    public AvatarService(StudentService studentService, AvatarRepository avatarRepository) {
         this.studentService = studentService;
         this.avatarRepository = avatarRepository;
     }
 
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
-        Student student = studentService.get(studentId);
+        Student student =studentService.get(studentId);
         Path filePath = Path.of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
         Files.createDirectories(filePath.getParent());
         Files.deleteIfExists(filePath);
@@ -53,22 +49,18 @@ public class AvatarService {
     }
 
     public Avatar findAvatar(Long studentId) {
-        return avatarRepository.findByStudentId(studentId).orElseThrow(EntityNotFoundException::new);
+        return avatarRepository.findByStudentId(studentId).orElseThrow(EntityNotFoundException::new );
     }
-
     private String getExtensions(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
-
     public Avatar findCreadAvatar(Long studentId) {
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
     }
-
-    public List<Avatar> findAvatarPage(Integer pageNumber, Integer pageSize) {
+    public List<Avatar> findAvatarPage(Integer pageNumber , Integer pageSize) {
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, pageSize);
         return avatarRepository.findAll(pageRequest).getContent();
     }
-
 
 }
 
